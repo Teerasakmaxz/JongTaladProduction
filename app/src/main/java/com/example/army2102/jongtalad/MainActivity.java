5abec5dafe4ad025a23999e1c57dfa4152d13040
teerasaksathu <maxz_drum@icloud.com>
2017-09-08 17:32:29 +0700

    fix bug btn slow

--- app/src/main/java/com/example/army2102/jongtalad/MainActivity.java
+++ app/src/main/java/com/example/army2102/jongtalad/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button btnRegisterPage;
     private Button btnLoginPage;
-    private TextView tvForgotPassword;
+    private TextView tvForgotPassword ,textView3,textView4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +25,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnRegisterPage.setOnClickListener(this);
         btnLoginPage.setOnClickListener(this);
         tvForgotPassword.setOnClickListener(this);
+//        textView3.setOnClickListener(this);
+//        textView4.setOnClickListener(this);
     }
 
     private void initInstances() {
         btnRegisterPage = (Button) findViewById(R.id.btnRegisterPage);
         btnLoginPage = (Button) findViewById(R.id.btnLoginPage);
         tvForgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
+//        textView3 = (TextView) findViewById(R.id.textView3);
+//        textView4 = (TextView) findViewById(R.id.textView4);
+
     }
 
     @Override
     public void onClick(View view) {
-        if (view == btnRegisterPage) {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        if (view == btnLoginPage) {
+            Intent intent = new Intent(getApplicationContext(), LockReservation.class);
             startActivity(intent);
-        } else if (view == btnLoginPage) {
-            Intent intent = new Intent(MainActivity.this, LockReservation.class);
+        } else if (view == btnRegisterPage) {
+            Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
             startActivity(intent);
         } else if (view == tvForgotPassword) {
             Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
